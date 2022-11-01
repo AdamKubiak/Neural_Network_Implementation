f10b75881d80cfb628b1206479d1b0a4c24e6b32
AdamKubiak <249480@student.pwr.edu.pl>
2022-11-01 23:46:39 +0100

    one_hot fnc

---
+++ one_hot.py
@@ -0,0 +1,20 @@
+from sklearn.preprocessing import OneHotEncoder
+import numpy as np
+
+def _onehot(y, n_classes):
+        """Koduje etykiety do postaci "gorącojedynkowej"
+
+        Parametry
+        ------------
+        y : tablica, wymiary = [n_próbek]
+            Wartości docelowe.
+
+        Zwraca
+        -----------
+        onehot : tablica, wymiary = (n_etykiet, n_próbek)
+
+        """
+        onehot = np.zeros((n_classes, y.shape[0]))
+        for idx, val in enumerate(y):
+            onehot[val, idx] = 1.
+        return onehot.T
